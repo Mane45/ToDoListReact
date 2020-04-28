ee949781224cf6234e86a05c049bb25a2c5841bb
Mane45 <sofyan.mane@gmail.com>
2020-04-28 23:37:53 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import TodoList from "./ToDo/TodoList";
+
+export default function App(){
+  const todos = [
+    {id:1, completed:false, title: 'Learn JS'},
+    {id:1, completed:false, title: 'Learn CSS'},
+    {id:1, completed:false, title: 'Learn React'}
+  ]
+
+  return (
+    <div className="App">
+      <h1>ToDO list</h1>
+      <TodoList todos={todos}></TodoList>
+    </div>
+  )
+};
+
+
+
