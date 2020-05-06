80e3a27ebc54e2e6cbbbdcb2d4ce0b5afbcf6bb1
Mane45 <54979197+Mane45@users.noreply.github.com>
2020-05-06 14:17:15 +0400

    Update ToDoItem.js

--- src/ToDo/ToDoItem.js
+++ src/ToDo/ToDoItem.js
@@ -4,6 +4,7 @@ import  './TodoList.css';
 const styles = {
     li:{
         display:'flex',
+        justifyContent:'space-between',
         alignItems:'center',
         padding:'.5rem 1rem',
         border:'1px solid #ccc',
