5c1e11715dc5f4ea343996345acbeb31dc393218
Sargis Avetisyan <sevunc24@gmail.com>
2020-06-03 03:18:01 +0400

    gost

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import TodoList from "./ToDo/TodoList";
 
-export default function App(){
+export default function App() {
   const todos = [
-    {id:1, completed:false, title: 'Learn JS'},
-    {id:1, completed:false, title: 'Learn CSS'},
-    {id:1, completed:false, title: 'Learn React'}
-  ]
+    { id: 1, completed: false, title: "Learn JS" },
+    { id: 1, completed: false, title: "Learn CSS" },
+    { id: 1, completed: false, title: "Learn React" },
+  ];
 
   return (
     <div className="App">
-      <h1>ToDO list</h1>
+      <h1>ToDO t</h1>
       <TodoList todos={todos}></TodoList>
     </div>
-  )
-};
-
-
-
+  );
+}
